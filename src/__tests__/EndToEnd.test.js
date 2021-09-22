d4bc670f2203bbfdbcbfcc3baec0579857fb18e5
EmLmr <emeline.coding@gmail.com>
2021-09-22 15:04:11 +0200

    added end-to-end test for feature 1 also refactored code to respect DRY principle

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,27 +1,46 @@
-// FEATURE 2 - End-to-end testing
+// End-to-end testing
 import puppeteer from 'puppeteer';
 
+let browser;
+let page;
+
+beforeAll(async () => {
+  jest.setTimeout(30000);
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.goto('http://localhost:3000/');
+  await page.waitForSelector('.city');
+});
+
+afterAll(() => {
+  browser.close();
+});
+
+// FEATURE 1 - bonus task
+describe('Filter events by city', () => {
+  // Scenario 1
+  test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+    const allEvents = await page.$('.event');
+    expect(allEvents).toBeDefined();
+  })
+
+  //Scenario 2
+  test('User should see a list of suggestions when they search for a city', async () => {
+    const searchCity = await page.$('.city');
+    await searchCity.type('input[name=value]', 'berlin');
+    const suggestions = await page.$('.suggestions')
+    expect(suggestions).toBeDefined();
+  })
+
+  // Scenario 3
+  test('User can select a city from the suggested list', async () => {
+    const cityListSuggestions = await page.$('.suggestions')
+    expect(cityListSuggestions).toBeDefined();
+  })
+});
+
+// FEATURE 2 
 describe('show/hide an event details', () => {
-
-  let browser;
-  let page;
-  beforeAll(async () => {
-    jest.setTimeout(30000);
-    browser = await puppeteer.launch({
-      // // Headless mode for real-time test in Chromium:
-      // headless: false,
-      // slowMo: 250,
-      // ignoreDefaultArgs: ['--disable-extensions']
-    });
-    page = await browser.newPage();
-    await page.goto('http://localhost:3000/');
-    await page.waitForSelector('.event');
-  });
-
-  afterAll(() => {
-    browser.close();
-  });
-
   // Scenario 1
   test('An event element is collapsed by default', async () => {
     const eventDetails = await page.$('.event-details');
