225ac28104c50a01f9e66b9368c6be4332ead53a
EmLmr <emeline.coding@gmail.com>
2021-10-12 13:18:28 +0200

    fixed brackets and quotes

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -1,47 +1,46 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { InfoAlert } from "./Alert";
+import { InfoAlert } from './Alert';
 
 class CitySearch extends Component {
     state = {
-        query: "",
+        query: '',
         suggestions: [],
         showSuggestions: false,
     };
 
-    handleInputChanged = event => {
+    handleInputChanged = (event) => {
         const value = event.target.value;
         this.setState({ showSuggestions: true });
-        const suggestions = this.props.locations.filter(location => {
+        const suggestions = this.props.locations.filter((location) => {
             return location.toUpperCase().indexOf(value.toUpperCase()) > -1;
         });
         if (suggestions.length === 0) {
             this.setState({
                 query: value,
-                infoText: "We can not find the city you are looking for. Please try another city",
+                infoText: 'We can not find the city you are looking for. Please try another city',
             });
         } else {
             return this.setState({
                 query: value,
                 suggestions,
-                infoText: "",
+                infoText: '',
             });
         }
     };
 
-    handleItemClicked = suggestion => {
+    handleItemClicked = (suggestion) => {
         this.setState({
             query: suggestion,
             showSuggestions: false,
-            infoText: "",
+            infoText: '',
         });
-        this.props.updateEvents(suggestion, 0);
+        this.props.updateEvents(suggestion);
     };
 
     render() {
         return (
             <div className="CitySearch">
-                <InfoAlert text={this.state.infoText} />
                 <input
                     type="text"
                     className="city"
@@ -52,13 +51,14 @@ class CitySearch extends Component {
                         this.setState({ showSuggestions: true });
                     }}
                 />
-                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: "none" }}>
-                    {this.state.suggestions.map(suggestion => (
+                <InfoAlert text={this.state.infoText} />
+                <ul className="suggestions" style={this.state.showSuggestions ? {} : { display: 'none' }}>
+                    {this.state.suggestions.map((suggestion) => (
                         <li key={suggestion} onClick={() => this.handleItemClicked(suggestion)}>
                             {suggestion}
                         </li>
                     ))}
-                    <li key="all" onClick={() => this.handleItemClicked("all")}>
+                    <li key="all" onClick={() => this.handleItemClicked('all')}>
                         <b>See all cities</b>
                     </li>
                 </ul>
