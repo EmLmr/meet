e44a3d1b15712c8f6ddf4f8dabfaa7b7dbb92f9b
EmLmr <emeline.coding@gmail.com>
2021-10-13 17:38:10 +0200

    minor code layout changes

--- src/Event.js
+++ src/Event.js
@@ -1,56 +1,47 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Badge, Button} from 'react-bootstrap';
+import { Container, Row, Badge, Button } from 'react-bootstrap';
 
 class Event extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showDetails: false,
-    };
-    this.toggleDetails = this.toggleDetails.bind(this);
-  }
-  toggleDetails() {
-    this.setState({
-      showDetails: !this.state.showDetails,
-    });
-  }
-  render() {
-    let event = this.props.event;
-    return (
-      <Container className="event event-container">
-        <Row>
-        <h2 className="event-title">{event.summary}</h2>
-        </Row>
-        <Row>
-        <Badge className="event-status">{event.status}</Badge>
-        </Row>
-        <Row className="basic-info">
-        <h5 className="event-location">{event.location}</h5>
-          <h5 className="event-scheduled-start">
-            From: {event.start.dateTime} ({event.start.timeZone})
-          </h5>
-          {this.state.showDetails === true 
-          && (
-             <h5 className="event-scheduled-end">
-             Ends: {event.end.dateTime} ({event.end.timeZone})
-             </h5>) 
-          && (
-            <p className="event-details">{event.description}</p>
-          )}
-        </Row>
-        <Row>
-        <Button bsPrefix="custom-btn" className="details-btn" onClick={this.toggleDetails}>
-          {!this.state.showDetails ? 'Show details' : 'Close'}
-        </Button>
-        </Row>
-        
-       
-        
-        
-      </Container>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDetails: false,
+        };
+        this.toggleDetails = this.toggleDetails.bind(this);
+    }
+
+    toggleDetails() {
+        this.setState({
+            showDetails: !this.state.showDetails,
+        });
+    }
+
+    render() {
+        let event = this.props.event;
+        return (
+            <Container className="event event-container">
+                <Row>
+                    <h2 className="event-title">{event.summary}</h2>
+                </Row>
+                <Row>
+                    <Badge className="event-status">{event.status}</Badge>
+                </Row>
+                <Row className="basic-info">
+                    <h5 className="event-location">{event.location}</h5>
+                    <h5 className="event-scheduled-start">
+                        From: {event.start.dateTime} ({event.start.timeZone})
+                    </h5>
+                    {this.state.showDetails === true && <p className="event-details">{event.description}</p>}
+                </Row>
+                <Row>
+                    <Button bsPrefix="custom-btn" className="details-btn" onClick={this.toggleDetails}>
+                        {!this.state.showDetails ? 'Show details' : 'Close'}
+                    </Button>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default Event;
