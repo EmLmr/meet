13b5218b7ffbfd3eb374924614db184c8ad41045
EmLmr <emeline.coding@gmail.com>
2021-10-16 17:14:54 +0200

    modified Event component layout

--- src/Event.js
+++ src/Event.js
@@ -17,21 +17,28 @@ class Event extends Component {
         });
     }
 
+    convertDate(date) {
+        const newDate = new Date(date).toString().slice(3, 21);
+        return newDate;
+    }
+
     render() {
         let event = this.props.event;
         return (
             <Container className="event event-container">
                 <Row>
-                    <h2 className="event-title">{event.summary}</h2>
+                    <h1 className="event-title">{event.summary}</h1>
                 </Row>
                 <Row>
                     <Badge className="event-status">{event.status}</Badge>
                 </Row>
                 <Row className="basic-info">
-                    <h5 className="event-location">{event.location}</h5>
-                    <h5 className="event-scheduled-start">
-                        From: {event.start.dateTime} ({event.start.timeZone})
-                    </h5>
+                    <h2 className="event-location">{event.location}</h2>
+                </Row>
+                <Row>
+                    <h2 className="event-scheduled-start">
+                        From: {this.convertDate(event.start.dateTime)} ({event.start.timeZone})
+                    </h2>
                     {this.state.showDetails === true && <p className="event-details">{event.description}</p>}
                 </Row>
                 <Row>
