73b4febb9b19f2006780d3b24ac61902a7d44e98
EmLmr <emeline.coding@gmail.com>
2021-10-20 16:23:14 +0200

    uncommented code relating to welcome screen

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, Responsive
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api'; // , checkToken, getAccessToken
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
-// import WelcomeScreen from './WelcomeScreen';
+import WelcomeScreen from './WelcomeScreen';
 import EventGenre from './EventGenre';
 
 import logo from './img/logo.png';
@@ -20,35 +20,23 @@ class App extends Component {
         locations: [],
         currentLocation: 'all',
         numberOfEvents: 32,
-        // showWelcomeScreen: undefined,
+        showWelcomeScreen: undefined,
     };
 
-    // async componentDidMount() {
-    //     this.mounted = true;
-    //     const accessToken = localStorage.getItem('access_token');
-    //     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
-    //     const searchParams = new URLSearchParams(window.location.search);
-    //     const code = searchParams.get('code');
-    //     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-    //     if ((code || isTokenValid) && this.mounted) {
-    //         getEvents().then((events) => {
-    //             if (this.mounted) {
-    //                 this.setState({ events, locations: extractLocations(events) });
-    //             }
-    //         });
-    //     }
-    // }
-
-    componentDidMount() {
+    async componentDidMount() {
         this.mounted = true;
-        getEvents().then((events) => {
-            if (this.mounted) {
-                this.setState({
-                    events: events.slice(0, this.state.numberOfEvents),
-                    locations: extractLocations(events),
-                });
-            }
-        });
+        const accessToken = localStorage.getItem('access_token');
+        const isTokenValid = (await checkToken(accessToken)).error ? false : true;
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = searchParams.get('code');
+        this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+        if ((code || isTokenValid) && this.mounted) {
+            getEvents().then((events) => {
+                if (this.mounted) {
+                    this.setState({ events, locations: extractLocations(events) });
+                }
+            });
+        }
     }
 
     componentWillUnmount() {
@@ -86,7 +74,7 @@ class App extends Component {
     };
 
     render() {
-        // if (this.state.showWelcomeScreen === undefined) return <div className="App" />;
+        if (this.state.showWelcomeScreen === undefined) return <div className="App" />;
 
         return (
             <Container className="App">
@@ -129,12 +117,12 @@ class App extends Component {
                 </div>
 
                 <EventList events={this.state.events} />
-                {/* <WelcomeScreen
+                <WelcomeScreen
                     showWelcomeScreen={this.state.showWelcomeScreen}
                     getAccessToken={() => {
                         getAccessToken();
                     }}
-                /> */}
+                />
             </Container>
         );
     }
